813cf5b44a73a24acab056899483f89be9aa05b8
Mayomat <matthieu.sirier@efrei.net>
2025-10-06 09:41:41 +0200

    Empty Bookshelf class with names for the functions

---
+++ Bookshelf.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Bookshelf {
+    List<Book> books;
+    List<User> users;
+
+    Bookshelf(List<Book> books, List<User> users) {
+        this.books = books;
+        this.users = users;
+    }
+
+    public void loanBook(int idBook, User user) {
+
+    }
+
+    public void returnBook(int idBook, User user) {
+
+    }
+
+    public void displayAvailableBooks() {
+
+    }
+}
